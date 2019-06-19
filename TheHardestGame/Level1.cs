afe64f816bd487a2987d5d2f149c319a99451447
Martin Gorgievski <martingorgievski05@gmail.com>
2019-06-19 03:12:46 +0200

    Add project files.

---
+++ TheHardestGame/Level1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TheHardestGame
+{
+    public partial class Level1 : Form
+    {
+        //public Square square;
+        public GameDoc gameDoc;
+        private Timer timer;
+        public Level1()
+        {   
+            InitializeComponent();
+            DoubleBuffered = true;
+            gameDoc = new GameDoc();
+            timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+            this.BackColor = Color.White;
+            
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            foreach(var item in gameDoc.balls)
+            {
+                item.MoveBall();
+                Invalidate(true);
+            }
+        }
+        private void Level1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush b = new SolidBrush(Color.Blue);
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            pen.Width = 5;
+            
+            e.Graphics.DrawLines(pen, gameDoc.points);
+            gameDoc.DrawBalls(e.Graphics);  
+            gameDoc.square.Draw(e.Graphics);
+        }
+
+        private void Level1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Up)
+            {
+                gameDoc.square.Move(Direction.UP);
+                Invalidate(true);
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                gameDoc.square.Move(Direction.DOWN);
+                Invalidate(true);
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                gameDoc.square.Move(Direction.LEFT);
+                gameDoc.isHitSquareLines();
+                Invalidate(true);
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                gameDoc.square.Move(Direction.RIGHT);
+                gameDoc.isHitSquareLines();
+                Invalidate(true);
+            }
+           
+        }
+    }
+}
---
+++ TheHardestGame/Square.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHardestGame
+{
+    public enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        UP_LEFT,
+        UP_RIGHT,
+        DOWN_LEFT,
+        DOWN_RIGHT
+    }
+    public class Square
+    {
+        public Point Position { get; set; }
+        public int X { get { return Position.X; } }
+        public int Y { get { return Position.Y; } }
+
+        private static int Speed = 15;
+
+
+        public Square()
+        {
+            Position = new Point(162, 162);
+        }
+
+        public void Draw(Graphics g)
+        {
+            SolidBrush b = new SolidBrush(Color.Orange);
+            g.FillRectangle(b, X, Y, 15, 15);
+            b.Dispose();
+        }
+        public void Move(Direction dir)
+        {
+            if(dir == Direction.RIGHT)
+            {
+                Position = new Point(X + 3, Y);
+            }
+            if(dir  == Direction.LEFT)
+            {
+                Position = new Point(X - 3, Y);
+            }
+            if(dir == Direction.UP)
+            {
+                Position = new Point(X, Y - 3);
+            }
+            if(dir == Direction.DOWN)
+            {
+                Position = new Point(X, Y + 3);
+            }
+           
+        }
+
+    }
+}
